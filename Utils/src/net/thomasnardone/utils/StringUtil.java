5b9cbcf59e2b56fdad9ebc6c86cedd26fa0b9d2a
Thomas Nardone <email@thomasnardone.net>
2012-12-20 18:45:26 -0500

    Add pluralize()

--- Utils/src/net/thomasnardone/utils/StringUtil.java
+++ Utils/src/net/thomasnardone/utils/StringUtil.java
@@ -9,6 +9,14 @@ public class StringUtil {
 		return s.replaceFirst(String.valueOf(s.charAt(0)), String.valueOf(Character.toLowerCase(s.charAt(0))));
 	}
 
+	public static String pluralize(final String s) {
+		if (s.endsWith("s") || s.endsWith("sh") || s.endsWith("ch") || s.endsWith("o")) {
+			return s + "es";
+		} else {
+			return s + "s";
+		}
+	}
+
 	public static String splitCamelCase(String s) {
 		s = s.replaceAll(
 				String.format("%s|%s|%s", "(?<=[A-Z])(?=[A-Z][a-z])", "(?<=[^A-Z])(?=[A-Z])", "(?<=[A-Za-z])(?=[^A-Za-z])"), " ");
