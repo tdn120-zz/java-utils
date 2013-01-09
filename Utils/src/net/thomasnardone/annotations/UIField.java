73bcfcd803cc6821b917d306e02ea82d8dc23345
Thomas Nardone <email@thomasnardone.net>
2013-01-09 12:44:48 -0500

    Initial commit

---
+++ Utils/src/net/thomasnardone/annotations/UIField.java
@@ -0,0 +1,16 @@
+package net.thomasnardone.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface UIField {
+	Type value() default Type.Text;
+
+	public enum Type {
+		Combo, Date, EditText, Spinner, Text
+	}
+}
