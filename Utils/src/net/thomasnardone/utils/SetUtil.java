559abecf1905d7b83bb7fd49dd09265bec091ba2
Thomas Nardone <email@thomasnardone.net>
2013-03-29 16:42:49 -0400

    Support multiple values

--- Utils/src/net/thomasnardone/utils/SetUtil.java
+++ Utils/src/net/thomasnardone/utils/SetUtil.java
@@ -4,9 +4,11 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class SetUtil {
-	public static final <T> Set<T> newSet(final T item) {
+	public static final <T> Set<T> newSet(final T... items) {
 		HashSet<T> set = new HashSet<T>();
-		set.add(item);
+		for (T item : items) {
+			set.add(item);
+		}
 		return set;
 	}
 }
