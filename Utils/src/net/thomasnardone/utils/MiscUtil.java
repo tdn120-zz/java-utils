d2fdf024dd0a2ff99c8325e5a9c51848afae7fd7
Thomas Nardone <email@thomasnardone.net>
2013-04-12 22:40:13 -0400

    intCompare

--- Utils/src/net/thomasnardone/utils/MiscUtil.java
+++ Utils/src/net/thomasnardone/utils/MiscUtil.java
@@ -1,6 +1,10 @@
 package net.thomasnardone.utils;
 
 public class MiscUtil {
+	public static int intCompare(final int i1, final int i2) {
+		return i1 > i2 ? 1 : i1 < i2 ? -1 : 0;
+	}
+
 	public static long toMillis(final long nanos) {
 		return nanos / 1000000l;
 	}
